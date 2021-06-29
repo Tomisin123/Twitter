d3daea0c1775480ccfe3aeaaad57ffdcab25d6c8
Tomisin Ogunfunmi <tomisin@fb.com>
2021-06-29 15:41:55 -0500

    Created Tweet model class

--- twitter/User.h
+++ twitter/User.h
@@ -20,6 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 // Create initializer
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
++ (NSMutableArray *)tweetsWithArray:(NSArray *)dictionaries;
 
 @end
 
